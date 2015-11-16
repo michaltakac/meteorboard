260ed7fe51d06d28afe213bca4cfdc92da1b796e
Michal Takac <takacmichal92@gmail.com>
2015-11-16 11:58:57 +0100

    added form states

--- src/client/forms/Page.react.js
+++ src/client/forms/Page.react.js
@@ -1,12 +1,10 @@
 import Component from 'react-pure-render/component';
 import DocumentTitle from 'react-document-title';
-import {Col, ButtonInput, Input, Row} from 'react-bootstrap';
+import {Col, ButtonInput, Input, OverlayTrigger, Row, Tooltip} from 'react-bootstrap';
 import React, {PropTypes} from 'react';
 import FooterTools from '../../common/components/FooterTools.react';
 import PageHeader from '../app/components/PageHeader.react';
 import Box from '../app/components/Box.react';
-//import Form from '../app/components/Form.react';
-import {FormattedHTMLMessage} from 'react-intl';
 //import {Link} from 'react-router';
 
 export default class Page extends Component {
@@ -19,6 +17,9 @@ export default class Page extends Component {
 
   render() {
     const {msg: {widgets: msg}} = this.props;
+
+    const tooltipFocus = <Tooltip>Tooltip on focus.</Tooltip>;
+    const tooltipHover = <Tooltip>Tooltip on hover.</Tooltip>;
 
     return (
       <DocumentTitle title={msg.title}>
@@ -34,47 +35,47 @@ export default class Page extends Component {
                    title='Basic form elements'
                    titleClass='box border orange'>
                 {/* Basic form example */}
-                <h3 className="form-title">Basic Example</h3>
-                <form role="form">
-                  <Input label="Email address" placeholder="Enter email" type="email" />
-                  <Input label="Password" placeholder="Password" type="password" />
-                  <Input label="Check me out" type="checkbox" />
-                  <ButtonInput className="btn btn-success" type="submit" value="Submit" />
+                <h3 className='form-title'>Basic Example</h3>
+                <form role='form'>
+                  <Input label='Email address' placeholder='Enter email' type='email' />
+                  <Input label='Password' placeholder='Password' type='password' />
+                  <Input label='Check me out' type='checkbox' />
+                  <ButtonInput className='btn btn-success' type='submit' value='Submit' />
                 </form>
 
-                <div className="separator"></div>
+                <div className='separator'></div>
 
                 {/* Inline form example */}
-                <h3 className="form-title">Inline form</h3>
-                <form className="form-inline" role="form">
-                  <Input placeholder="Enter email" type="email" />
-                  <Input placeholder="Password" type="password" />
-                  <ButtonInput className="btn btn-inverse" type="submit" value="Sign in" />
+                <h3 className='form-title'>Inline form</h3>
+                <form className='form-inline' role='form'>
+                  <Input placeholder='Enter email' type='email' />
+                  <Input placeholder='Password' type='password' />
+                  <ButtonInput className='btn btn-inverse' type='submit' value='Sign in' />
                 </form>
 
-                <div className="separator"></div>
+                <div className='separator'></div>
 
                 {/* Horizontal form example */}
-                <h3 className="form-title">Horizontal form</h3>
-                <form className="form-horizontal" role="form">
-                  <Input label="Email"
-                         labelClassName="col-sm-2 control-label"
-                         placeholder="Email"
-                         type="email"
-                         wrapperClassName="col-sm-10" />
-                  <Input label="Password"
-                         labelClassName="col-sm-2 control-label"
-                         placeholder="Password"
-                         type="password"
-                         wrapperClassName="col-sm-10" />
-                  <Input help="Offset is applied to wrapper."
-                         label="Remember me"
-                         type="checkbox"
-                         wrapperClassName="col-sm-offset-2 col-sm-10" />
-                  <ButtonInput className="btn btn-pink"
-                               type="submit"
-                               value="Sign in"
-                               wrapperClassName="col-sm-offset-2 col-sm-10" />
+                <h3 className='form-title'>Horizontal form</h3>
+                <form className='form-horizontal' role='form'>
+                  <Input label='Email'
+                         labelClassName='col-sm-2 control-label'
+                         placeholder='Email'
+                         type='email'
+                         wrapperClassName='col-sm-10' />
+                  <Input label='Password'
+                         labelClassName='col-sm-2 control-label'
+                         placeholder='Password'
+                         type='password'
+                         wrapperClassName='col-sm-10' />
+                  <Input help='Offset is applied to wrapper.'
+                         label='Remember me'
+                         type='checkbox'
+                         wrapperClassName='col-sm-offset-2 col-sm-10' />
+                  <ButtonInput className='btn btn-pink'
+                               type='submit'
+                               value='Sign in'
+                               wrapperClassName='col-sm-offset-2 col-sm-10' />
                 </form>
               </Box>
 
@@ -84,13 +85,13 @@ export default class Page extends Component {
                    titleClass='box border purple'>
                 <Row>
                   <Col xs={2}>
-                    <Input placeholder=".col-xs-2" type="text" />
+                    <Input placeholder='.col-xs-2' type='text' />
                   </Col>
                   <Col xs={3}>
-                    <Input placeholder=".col-xs-3" type="text" />
+                    <Input placeholder='.col-xs-3' type='text' />
                   </Col>
                   <Col xs={4}>
-                    <Input placeholder=".col-xs-4" type="text" />
+                    <Input placeholder='.col-xs-4' type='text' />
                   </Col>
                 </Row>
               </Box>
@@ -102,59 +103,59 @@ export default class Page extends Component {
                    title='Form controls'
                    titleClass='box border blue'>
                 {/* Inline form example */}
-                <h3 className="form-title">Supported controls</h3>
-                <form className="form-horizontal" role="form">
-                  <Input label="Inputs"
-                         labelClassName="col-sm-3 control-label"
-                         placeholder="Text input"
-                         type="text"
-                         wrapperClassName="col-sm-9" />
-                  <Input label="Textarea"
-                         labelClassName="col-sm-3 control-label"
-                         type="textarea"
-                         wrapperClassName="col-sm-9" />
-                  <Input label="Inline checkboxes" labelClassName="col-sm-3 control-label" wrapperClassName="col-sm-9">
+                <h3 className='form-title'>Supported controls</h3>
+                <form className='form-horizontal' role='form'>
+                  <Input label='Inputs'
+                         labelClassName='col-sm-3 control-label'
+                         placeholder='Text input'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                  <Input label='Textarea'
+                         labelClassName='col-sm-3 control-label'
+                         type='textarea'
+                         wrapperClassName='col-sm-9' />
+                  <Input label='Inline checkboxes' labelClassName='col-sm-3 control-label' wrapperClassName='col-sm-9'>
                     <Row>
-                      <Col className="col-xs-offset-1" xs={2}>
-                        <Input label="1"
-                               labelClassName="checkbox-inline"
-                               type="checkbox" />
+                      <Col className='col-xs-offset-1' xs={2}>
+                        <Input label='1'
+                               labelClassName='checkbox-inline'
+                               type='checkbox' />
                       </Col>
                       <Col xs={2}>
-                        <Input label="2"
-                               labelClassName="checkbox-inline"
-                               type="checkbox" />
+                        <Input label='2'
+                               labelClassName='checkbox-inline'
+                               type='checkbox' />
                       </Col>
                       <Col xs={2}>
-                        <Input label="3"
-                               labelClassName="checkbox-inline"
-                               type="checkbox" />
+                        <Input label='3'
+                               labelClassName='checkbox-inline'
+                               type='checkbox' />
                       </Col>
                     </Row>
                   </Input>
-                  <Input label="Select"
-                         labelClassName="col-sm-3 control-label"
-                         placeholder="select"
-                         type="select"
-                         wrapperClassName="col-sm-9">
-                    <option value="select">select</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+                  <Input label='Select'
+                         labelClassName='col-sm-3 control-label'
+                         placeholder='select'
+                         type='select'
+                         wrapperClassName='col-sm-9'>
+                    <option value='select'>select</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
                   </Input>
-                  <Input label="Multiple Select"
-                         labelClassName="col-sm-3 control-label"
-                         type="select"
-                         wrapperClassName="col-sm-9"
-                         multiple>
-                    <option value="select">select (multiple)</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+                  <Input label='Multiple Select'
+                         labelClassName='col-sm-3 control-label'
+                         multiple
+                         type='select'
+                         wrapperClassName='col-sm-9'>
+                    <option value='select'>select (multiple)</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
                   </Input>
                 </form>
               </Box>
@@ -162,8 +163,51 @@ export default class Page extends Component {
               <Box bodyClass='box-body big'
                    icon='fa fa-bars'
                    title='Form states'
-                   titleClass='box border orange'>
-                   ydsdbdbsdb
+                   titleClass='box border green'>
+                <h3 className='form-title'>Form states</h3>
+                <form className='form-horizontal' role='form'>
+                  <Input label='Input focus'
+                         labelClassName='col-sm-3 control-label'
+                         placeholder='This is focused...'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                  <Input disabled
+                         label='Disabled inputs'
+                         labelClassName='col-sm-3 control-label'
+                         placeholder='Disabled input here...'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                  <OverlayTrigger overlay={tooltipFocus} placement='top' trigger='click'>
+                    <Input label='Field with tooltip on focus'
+                           labelClassName='col-sm-3 control-label'
+                           type='text'
+                           wrapperClassName='col-sm-9' />
+                  </OverlayTrigger>
+                  <OverlayTrigger overlay={tooltipHover} placement='top'>
+                    <Input label='Field with tooltip on hover'
+                           labelClassName='col-sm-3 control-label'
+                           type='text'
+                           wrapperClassName='col-sm-9' />
+                  </OverlayTrigger>
+                  <Input bsStyle='success'
+                         hasFeedback
+                         label='Input with success'
+                         labelClassName='col-sm-3 control-label'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                  <Input bsStyle='warning'
+                         hasFeedback
+                         label='Input with warning'
+                         labelClassName='col-sm-3 control-label'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                  <Input bsStyle='error'
+                         hasFeedback
+                         label='Input with error'
+                         labelClassName='col-sm-3 control-label'
+                         type='text'
+                         wrapperClassName='col-sm-9' />
+                </form>
               </Box>
             </Col>
           </Row>
